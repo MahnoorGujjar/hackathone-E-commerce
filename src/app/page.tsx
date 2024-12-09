ba47737c86532023f2ebbce1ff9b3be53d32e519
MahnoorGujjar <mahanooray1@gmail.com>
2024-12-09 01:39:43 -0800

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,22 @@
+
+import Hero from "./components/Hero";
+import Hero1 from "./components/Hero1";
+import Featured from "./components/Featured";
+import Gear from "./components/Gear-up";
+import Miss from "./components/Miss";
+import Essential from "./components/Essential";
+
+
+export default function Home() {
+  return (
+    <div>
+      <Hero />
+      <Hero1 />
+      <Featured />
+      <Gear />
+      <Miss />
+      <Essential />
+      
+    </div>
+  );
+}
